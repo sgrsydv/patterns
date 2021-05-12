e619e20e566512059eb9bf5008e67b94d411464b
Sagar Yadav <>
2021-05-12 17:54:31 +0530

    Added image

--- pages/abstract-factory-pattern/index.js
+++ pages/abstract-factory-pattern/index.js
@@ -15,17 +15,45 @@ export default function FirstPost() {
 
       <main>
         <section class="text-gray-600 body-font">
-          <div class="container py-24 mx-auto">
+          <div class="container py-24 px-5 mx-auto pb-0">
             <div class="text-center">
-              <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Abstract Factory pattern</h1>
-              <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">The abstract factory pattern provides a way to encapsulate a group of individual factories that have a common theme without specifying their concrete classes.</p>
+              <h1 class="sm:text-4xl text-3xl font-medium title-font text-gray-900 mb-4">Abstract Factory pattern</h1>
+              <p class="text-lg leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">The abstract factory pattern provides a way to encapsulate a group of individual factories that have a common theme without specifying their concrete classes.</p>
               <div class="flex mt-6 justify-center">
                 <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
               </div>
             </div>
+          </div>
+        </section>
+        
+        {/* <section class="text-gray-600 body-font">
+          <div class="container mx-auto flex  md:flex-row flex-col items-center">
+            <div class="lg:max-w-3xl lg:w-full mx-auto">
+              <img class="rounded" alt="hero" src="/abstract-factory-pattern.svg"/>
+            </div>
+          </div>
+        </section> */}
 
-            {/* <div class="flex flex-wrap w-full">
-              <div class="lg:w-2/5 md:w-1/2 md:pr-1 md:py-6 mx-auto">
+        <section class="text-gray-600 body-font">
+          <div class="container mx-auto px-5 flex items-center justify-center flex-col">
+            <div class="lg:max-w-3xl lg:w-full mx-auto">
+              <img class="rounded" alt="hero" src="/abstract-factory-pattern.svg"/>
+            </div>
+            <div class="lg:w-3/6 w-full">
+              <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Description</h1>
+              <p class="text-lg mb-8 leading-relaxed">The abstract factory pattern provides a way to encapsulate a group of individual factories that have a common theme without specifying their concrete classes. In normal usage, the client software creates a concrete implementation of the abstract factory and then uses the generic interface of the factory to create the concrete objects that are part of the theme. The client does not know (or care) which concrete objects it gets from each of these internal factories, since it uses only the generic interfaces of their products. This pattern separates the details of implementation of a set of objects from their general usage and relies on object composition, as object creation is implemented in methods exposed in the factory interface.</p>
+              {/* <div class="flex justify-center">
+                <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+              </div> */}
+            </div>
+          </div>
+        </section>
+
+        <section class="text-gray-600 body-font">
+          <div class="container mx-auto px-5 flex flex-wrap">
+            <div class="flex flex-wrap w-full">
+              <div class="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6 mx-auto">
                 <div class="flex relative pb-12">
                   <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
                     <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
@@ -97,20 +125,15 @@ export default function FirstPost() {
                   </div>
                 </div>
               </div>
-            </div> */}
-
-
+            </div>
           </div>
         </section>
-        {/* <section class="mx-auto">
-          <Image src="/abstract-factory-pattern.svg" layout="fill" objectFit="cover" />
-        </section> */}
-        
+
         <section class="text-gray-600 body-font">
-          <div class="container mx-auto flex  md:flex-row flex-col items-center">
-            
-            <div class="lg:max-w-3xl lg:w-full mx-auto">
-              <img class="rounded" alt="hero" src="/abstract-factory-pattern.svg"/>
+          <div class="container px-5 py-24 mx-auto">
+            <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+              <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">Source: Wikipedia</h1>
+              <button class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">VISIT</button>
             </div>
           </div>
         </section>
