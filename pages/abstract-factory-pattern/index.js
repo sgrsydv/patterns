6b6508693a98b56b2eb19fcbcead994305742d7d
Sagar Yadav <>
2021-05-12 22:17:50 +0530

    Fixed few issues

--- pages/abstract-factory-pattern/index.js
+++ pages/abstract-factory-pattern/index.js
@@ -132,7 +132,7 @@ export default function FirstPost() {
         <section class="text-gray-600 body-font">
           <div class="container px-5 py-24 mx-auto">
             <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-              <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">Wikipedia(https://en.wikipedia.org/wiki/Abstract_factory_pattern)</h1>
+              <h1 class="overflow-ellipsis overflow-hidden sm:pr-16 text-2xl font-medium title-font text-gray-900">Wikipedia</h1>
               <button class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">VISIT</button>
             </div>
           </div>
