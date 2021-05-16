9f5d7786fea750851a7381dbe58e57ccd5e66292
Sagar Yadav <>
2021-05-16 13:35:59 +0530

    Improvements

--- pages/abstract-factory-pattern/index.js
+++ pages/abstract-factory-pattern/index.js
@@ -43,7 +43,7 @@ export default function FirstPost() {
               <img class="rounded" alt="Abstract-factory-pattern" src="/abstract-factory-pattern.svg"/>
             </div>
             <div class="lg:w-2/5 w-full">
-              <h1 class="title-font sm:text-3xl text-2xl mb-4 font-medium text-gray-900">Description</h1>
+              <h1 class="title-font sm:text-3xl text-2xl font-medium text-gray-900">Description</h1>
               <p class="text-lg mb-8 leading-relaxed">The abstract factory pattern provides a way to encapsulate a group of individual factories that have a common theme without specifying their concrete classes. In normal usage, the client software creates a concrete implementation of the abstract factory and then uses the generic interface of the factory to create the concrete objects that are part of the theme. The client does not know (or care) which concrete objects it gets from each of these internal factories, since it uses only the generic interfaces of their products. This pattern separates the details of implementation of a set of objects from their general usage and relies on object composition, as object creation is implemented in methods exposed in the factory interface.</p>
               {/* <div class="flex justify-center">
                 <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
