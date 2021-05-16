710b4a77690bd38878c3cdeae31dc551bf0d9261
Sagar Yadav <>
2021-05-16 22:47:49 +0530

    Improvements

--- pages/factory-method-pattern/index.js
+++ pages/factory-method-pattern/index.js
@@ -13,6 +13,22 @@ export default function FirstPost() {
       </Head>
 
       <main>
+        <div class="fixed bottom-0 lg:left-0 lg:inset-y-1/3 w-14 h-14 md:w-20 md:h-20 my-5 mx-10 bg-gray-100 rounded-full grid self-center">
+          <Link href="/abstract-factory-pattern">
+            <a class="flex title-font font-medium items-center text-gray-900 mb-4">
+              <img class="rounded px-5 py-5 md:px-7 md:py-7" alt="Lightbulb" src="/previous.svg" />
+            </a>
+          </Link>
+        </div>
+
+        <div class="fixed right-0 bottom-0 lg:inset-y-1/3 w-14 h-14 md:w-20 md:h-20 my-5 mx-10 bg-gray-100 rounded-full grid self-center">
+          <Link href="">
+            <a class="flex title-font font-medium items-center text-gray-900 mb-4">
+              <img class="rounded px-5 py-5 md:px-7 md:py-7" alt="Lightbulb" src="/next.svg" />
+            </a>
+          </Link>
+        </div>
+
         <section class="text-gray-600 body-font">
           <div class="container px-5 py-24 mx-auto">
             <div class="text-center mb-20">
